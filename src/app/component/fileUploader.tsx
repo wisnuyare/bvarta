68db254f37a70503914923d96c2709f55c7af899
wisnuyare <wisnuyare@gmail.com>
2024-04-15 21:26:49 +0700

    initial commit:UI for map and upload geojson file

---
+++ src/app/component/fileUploader.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React, { useState } from 'react';
+
+interface FileUploaderProps {
+  onFileUpload: (geoJSONData: any) => void;
+}
+
+const FileUploader: React.FC<FileUploaderProps> = ({ onFileUpload }) => {
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files && event.target.files[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+      readGeoJSONFile(selectedFile);
+    }
+  };
+
+  const readGeoJSONFile = (file: File) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      if (event.target && event.target.result) {
+        const geoJSONData = JSON.parse(event.target.result as string);
+        onFileUpload(geoJSONData);
+      }
+    };
+    reader.readAsText(file);
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept=".geojson"
+        onChange={handleFileChange}
+      />
+    </div>
+  );
+};
+
+export default FileUploader;
---
+++ src/app/component/mapVisualize.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon.src,
+    iconRetinaUrl: markerIcon2x.src,
+    shadowUrl: markerShadow.src,
+})
+
+interface MapVisualizeProps {
+    geoJSONData: any
+  }
+
+const MapVisualize: React.FC<MapVisualizeProps> = (geoJSONData : any) => {
+    useEffect(() => {
+        if (geoJSONData.geoJSONData) {
+          const map = L.map('map').setView([0, 0], 2);
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          }).addTo(map);
+          L.geoJSON(geoJSONData.geoJSONData).addTo(map);
+          map.fitBounds(L.geoJSON(geoJSONData.geoJSONData).getBounds());
+        }
+      }, [geoJSONData]);
+
+  return (
+    <div>
+      <div id="map" style={{ height: '400px' }} />
+      {geoJSONData.geoJSONData && (
+        <div>
+          <h2>GeoJSON Data:</h2>
+          <pre>{JSON.stringify(geoJSONData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MapVisualize;
